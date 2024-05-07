a40b1b88277815e48a7ad21adec6830db091c9ab
Artem0202 <artyomkov.19@gmail.com>
2024-05-07 20:26:14 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function getElementWidth(content, padding, border) {
-  return `${
+  return (
     Number.parseFloat(content) +
     2 * (Number.parseFloat(padding) + Number.parseFloat(border))
-  }`;
+  );
 }
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
